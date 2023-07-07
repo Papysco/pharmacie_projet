5676a1132c0b7694e20d9f58de422b9a12490383
papysco <papyscofall@gmail.com>
2023-07-07 23:10:43 +0000

    test de connection pour pharmacien

--- client/src/composants/login.js
+++ client/src/composants/login.js
@@ -1,88 +1,132 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "../style/login.css";
 import logo from "../images/Pharmacie.svg";
 
-function Login() {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState(false);
-
-  const handleLogin = () => {
-    // Votre logique de connexion ici
-
-    // Exemple d'erreur de connexion
-    setError(true);
+class Login extends Component {
+  state = {
+    login: "",
+    password: "",
+    error: false,
+    info_login: [
+      { login: "papyscofall@gmail.com", password: "1234" },
+      { login: "papysco@gmail.com", password: "1234" },
+      { login: "papyfall@gmail.com", password: "1234" },
+    ],
   };
 
-  return (
-    <section className="h-100 d-flex align-items-center">
-      <div className="container">
-        <div className="row justify-content-center">
-          <div className="col-lg-6">
-            <div className="card rounded-3 text-black">
-              <div className="card-body p-md-5 mx-md-4">
-                <div className="text-center">
-                  <img
-                    src={logo}
-                    style={{ width: "350px", height: "250px" }}
-                    alt="logo"
-                  />
-                </div>
+  handleLogin = (event) => {
+    event.preventDefault();
+    const { login, password } = this.state;
+    let found = false;
 
-                <form>
-                  <div className="form-outline mb-4">
-                    <label className="form-label" htmlFor="form2Example11">
-                      Identifiant
-                    </label>
-                    <input
-                      type="email"
-                      id="identifiant"
-                      className="form-control"
-                      placeholder="adresse e-mail"
-                      required
+    for (let i = 0; i < this.state.info_login.length; i++) {
+      let element = this.state.info_login[i];
+
+      if (element.login == login && element.password == password) {
+        found = true;
+      }
+    }
+
+    found == true
+      ? this.setState({ error: false })
+      : this.setState({ error: true });
+  };
+
+  onChangeLogin = (event) => {
+    this.setState({ login: event.target.value });
+  };
+
+  onChangePassword = (event) => {
+    this.setState({ password: event.target.value });
+  };
+
+  handleError = () => {
+    const { error } = this.state;
+
+    if (error === true) {
+      return (
+        <div
+          className="alert alert-danger d-flex align-items-center"
+          role="alert"
+        >
+          <div>Identifiant ou mot de passe incorrect !</div>
+        </div>
+      );
+    } else {
+      return;
+    }
+  };
+
+  render() {
+    const { error } = this.state;
+
+    return (
+      <section className="h-100 d-flex align-items-center">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-6">
+              <div className="card rounded-3 text-black">
+                <div className="card-body p-md-5 mx-md-4">
+                  <div className="text-center">
+                    <img
+                      src={logo}
+                      style={{ width: "350px", height: "250px" }}
+                      alt="logo"
                     />
                   </div>
 
-                  <div className="form-outline mb-4">
-                    <label className="form-label" htmlFor="form2Example22">
-                      Mot de passe
-                    </label>
-                    <input
-                      type="password"
-                      id="password"
-                      placeholder=""
-                      className="form-control"
-                      aria-required
-                    />
-                  </div>
+                  <form>
+                    <div className="form-outline mb-4">
+                      <label className="form-label" htmlFor="form2Example11">
+                        Identifiant
+                      </label>
+                      <input
+                        type="email"
+                        id="identifiant"
+                        className="form-control"
+                        placeholder="adresse e-mail"
+                        required
+                        value={this.state.login}
+                        onChange={this.onChangeLogin}
+                      />
+                    </div>
 
-                  <div className="text-center pt-2 mb-5 pb-1">
-                    <button
-                      className="btn btn-primary btn-block fa-lg mb-3 blue_button"
-                      type="button"
-                      onClick={handleLogin}
-                    >
-                      Se connecter
-                    </button>
-                    <br />
-                    <br />
-                    {error && (
-                      <div
-                        className="alert alert-danger d-flex align-items-center"
-                        role="alert"
+                    <div className="form-outline mb-4">
+                      <label className="form-label" htmlFor="form2Example22">
+                        Mot de passe
+                      </label>
+                      <input
+                        type="password"
+                        id="password"
+                        placeholder=""
+                        className="form-control"
+                        aria-required
+                        value={this.state.password}
+                        onChange={this.onChangePassword}
+                      />
+                    </div>
+
+                    <div className="text-center pt-2 mb-5 pb-1">
+                      <button
+                        className="btn btn-primary btn-block fa-lg mb-3 blue_button"
+                        type="button"
+                        onClick={this.handleLogin}
                       >
-                        <div>Identifiant ou mot de passe incorrect !</div>
-                      </div>
-                    )}
-                  </div>
-                </form>
+                        Se connecter
+                      </button>
+                      <br />
+                      <br />
+                      {this.handleError()}
+                    </div>
+                  </form>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
+  }
 }
 
 export default Login;
